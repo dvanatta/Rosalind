4c5fe1ee5838433f06bfed0e2187674cd6e233ef
Dan <Dan@Jenns-MacBook-Pro.local>
2014-10-07 10:49:44 -0700

    fixed dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -1,69 +1,61 @@
-# for 2,2 starting at 1,3,2 should be 13
-# need to store position, orientation, and score
-# output = [i,j,score,[t,s,w]]
+# need to store orientation and score for each vert
 import numpy
 
+#input [Top, South, East]
 die = [1,3,2]
-
-  
-#output_array
+ 
+ 
 def initialize(x,y):
+    vert_list = []
+    for i in range(x):
+        for j in range(y):
+            vert_list.append([i,j])
     scores = numpy.zeros((x,y))+float('inf')
-    scores[0,0] = 1
+    scores[0,0] = die[0]
     dice = []  
     for i in range(x):
         dice.append([[0,0,0]]*y)
-        for j in range(y+1):
-             pass        
-#            v[(i,j)]= [[]]
-#            v.append([[i,j],[]])
-#            scores.append(float('inf'))
     dice[0][0] = die
-#    print dice[0][0], scores[0,0]
+    return vert_list,dice, scores
 
-#    print dice[0][0]
-    return dice, scores
 
 def move_up(x):
     return [x[1],7-x[0],x[2]]
+
 
 def move_right(x):
     return [x[2],x[1],7-x[0]]
 
 
-#look at pseudo code, you might need some equivalent for v. 
+def lowest_unchecked(vert_list, scores):
+    min_val = float('inf')
+    i,j = 0,0
+    for vert in vert_list:
+        if scores[(vert[0],vert[1])] < min_val:
+            min_val = scores[(vert[0],vert[1])]
+            i,j = vert[0],vert[1]
+    return min_val, i, j
+        
+
 def run(x,y):
-    dice, scores = initialize(x,y)
-    while scores.max() == float('inf'):
-        lowest_score = scores.min()
-        print scores, scores.min()
-        i,j = numpy.where(scores == lowest_score)
-        print numpy.where(scores == lowest_score)
-#        scores[i,j] = 100000
-        print i, x
+    x += 1 #Oops, implemented for starting at 1,1 instead of 0,0
+    y += 1
+    vert_list, dice, scores = initialize(x,y)
+    while vert_list:
+        lowest_score, i, j = lowest_unchecked(vert_list,scores)
+        vert_list.remove([i,j])        
         if i+1 < x:
-                die1 = move_right(dice[i][j])
-                if scores[i+1,j] >= scores[i,j] + die1[0]:
-                    scores[i+1,j] = scores[i,j] + die1[0]
-                    dice[i+1][j] = die1
-                    scores[i,j] = 1000000  # OH GOD THIS IS TERRIBLE I'M SO SORRY
-                    pass
+            die1 = move_right(dice[i][j])
+            if scores[i+1,j] >= scores[i,j] + die1[0]:
+                scores[i+1,j] = scores[i,j] + die1[0]
+                dice[i+1][j] = die1
         if j+1 < y:
-                die1 = move_up(dice[i][j])
-                if scores[i,j+1] >= scores[i,j] + die1[0]:
-                    scores[i,j+1] = scores[i,j] + die1[0]
-                    dice[i][j+1] = die1
-                    scores[i,j] = 1000000  # OH GOD THIS IS TERRIBLE I'M SO SORRY
-                    pass
-#            die1 = move_up(die)
-#            die2 = move_right(die)
-        print x,y
-        if scores[x-1,y-1] != float('inf'): 
-            break
+            die1 = move_up(dice[i][j])
+            if scores[i,j+1] >= scores[i,j] + die1[0]:
+                scores[i,j+1] = scores[i,j] + die1[0]
+                dice[i][j+1] = die1
     print scores, lowest_score        
-    return scores.min()    
+    return lowest_score    
 
-#init_np(2,2)
-print run(5,5)
-
+print run(2,3)
 
