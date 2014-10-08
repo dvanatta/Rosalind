0c270156c9c2ee6db9787011bc6b26307f2ed60c
Dan <Dan@Jenns-MacBook-Pro.local>
2014-10-08 11:57:09 -0700

    pop scripts

---
+++ pop_scripts/iev.py
@@ -0,0 +1,7 @@
+def iev(array, weights = [1, 1, 1, .75, .5, 0]):
+    result = 0
+    for i in range(len(array)):
+        result += array[i]*weights[i]
+    return result*2
+
+print iev([18032, 19982, 17084, 17192, 17430, 18315])
---
+++ pop_scripts/rabbits.py
@@ -0,0 +1,10 @@
+def fib(n,k):
+   if n <= 2:
+      return 1
+   else:
+      return fib(n-1, k) + k*fib(n-2,k)
+
+print fib(32,4)
+
+
+
