ce540182f664587d4ac9da31239aac0f2a5f0ce7
Dan Vanatta <dvanatta@sr12-2c2f6916aa.stanford.edu>
2014-10-27 14:08:32 -0700

    setting up travis CI

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='dna_tools',
+      url='http://github.com/dvanatta/rosalind',
+      version='1.0',
+      description='Tools for bioinformatic analysis',
+      author='Dan Vanatta',
+      author_email='dvanatta@stanford.edu',
+      py_modules=['dna', 'dna_parser'],
+      )
