7fbdc2caa20ed603bab934d5ba291d1cce101c76
Dan Vanatta <dvanatta@stanford.edu>
2014-11-04 16:36:53 -0800

    added tests

--- subseq.py
+++
@@ -1,5 +0,0 @@
-def longest_increasing(seq):
-    value = seq[0]
-    i
-
-longest_increasing([
--- unit_test_dna.py
+++ unit_test_dna.py
@@ -1,25 +1,21 @@
 import unittest
-import dna 
+import dna
 import dna_parser
-#maybe roll this into a parser test?
-#include parser test on simple files for both txt and rosalind
+# include parser test on simple files for both txt and rosalind
 
 dataset = dna_parser.parse_rosalind("Examples/ex5.txt")
 
-class DNATests(unittest.TestCase):
 
+class DNATests(unittest.TestCase):
 
     def test_counting_nucs(self):
         self.assertEqual((20, 12, 17, 21), dna.count('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'))
- 
 
     def test_rna_sense(self):
         self.assertEqual('GAUGGAACUUGACUACGUAAAUU', dna.to_rna_sense('GATGGAACTTGACTACGTAAATT'))
 
-
     def test_dna_rev_comp(self):
         self.assertEqual('ACCGGGTTTT', dna.rev_comp('AAAACCCGGT', "dna"))
-
 
     def test_rna_rev_comp(self):
         self.assertEqual('ACCGGGUUUU',dna.rev_comp('AAAACCCGGT', "rna"))
