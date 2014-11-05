4a19f4daeb6dff55a01aa1a6cb1afa57f27de04d
Dan Vanatta <dvanatta@stanford.edu>
2014-11-05 12:05:55 -0800

    fixed line spacing

--- unit_test_dna.py
+++ unit_test_dna.py
@@ -8,20 +8,26 @@ dataset = dna_parser.parse_rosalind("Examples/ex5.txt")
 
 class DNATests(unittest.TestCase):
 
+
     def test_counting_nucs(self):
         self.assertEqual((20, 12, 17, 21), dna.count('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'))
+
 
     def test_rna_sense(self):
         self.assertEqual('GAUGGAACUUGACUACGUAAAUU', dna.to_rna_sense('GATGGAACTTGACTACGTAAATT'))
 
+
     def test_dna_rev_comp(self):
         self.assertEqual('ACCGGGTTTT', dna.rev_comp('AAAACCCGGT', "dna"))
+
 
     def test_rna_rev_comp(self):
         self.assertEqual('ACCGGGUUUU',dna.rev_comp('AAAACCCGGT', "rna"))
 
+
     def test_gc(self):
         self.assertAlmostEqual(.60919540, dna.calculate_gc("CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT"),4)
+
 
     def test_hamming(self):
         self.assertEqual(7, dna.calculate_hamming('GAGCCTACTAACGGGAT','CATCGTAATGACGGCCT'))
@@ -30,11 +36,18 @@ class DNATests(unittest.TestCase):
     def test_translate(self):
         self.assertEqual("MAMAPRTEINSTRING", dna.translate("AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"))
 
+
     def test_motif(self):
         self.assertEqual([2, 4, 10], dna.motif("GATATATGCATATACTT", "ATAT"))    
+   
+   
     def test_consensus(self):
         pass
+   
+   
     def test_longest_common(self):
         pass
+
+
 if __name__ == '__main__':
     unittest.main()
