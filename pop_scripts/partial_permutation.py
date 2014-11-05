f649b7e2622dd9fb0aae944280da49b61e17b1c0
Dan Vanatta <dvanatta@stanford.edu>
2014-11-04 16:07:10 -0800

    organized main dir

---
+++ pop_scripts/partial_permutation.py
@@ -0,0 +1,14 @@
+import math
+n = 91 
+k = 10 
+
+def binomial_coefficient(n,k):
+   if k > n / 2:
+      k = n - k
+   result = 1.0
+   for i in range(1,k + 1):
+      result *= ((n - (k - i)) / float(i))
+   return result
+print (math.factorial(n) / math.factorial(n-k)) % 1000000
+
+#print binomial_coefficient(n,k) % 1000000
---
+++ wrappers/wrap_enum_lex.py
@@ -0,0 +1,5 @@
+import dna
+
+output = dna.enum_lex(3, ["N", "J", "X", "I", "K", "R", "D", "V", "C"])
+for i in output:
+    print i
